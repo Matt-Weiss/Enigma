614c7b3d48bfe565548348f382acb2d0e2a33b30
Matt Weiss <weiss.matt@outlook.com>
2019-02-23 21:44:27 -0700

    Runner seems to work as directed.

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,3 +1,8 @@
+require './lib/runner'
+
 if ARGV.length != 4
   puts "Expected 4 arguments, recieved #{ARGV.length}"
+  exit
 end
+
+Runner.start(ARGV)
--- lib/runner.rb
+++ lib/runner.rb
@@ -2,12 +2,42 @@ require './lib/enigma'
 
 class Runner
 
+  def self.read_message_from_file(file_name)
+    file_path = "./data/#{file_name}"
+    file = File.new(file_path)
+    @input = file.read.chomp
+  end
 
+  def self.create_write_file(file_name)
+    @output_name = file_name
+    file_path = "./data/#{file_name}"
+    @output_file = File.open(file_path, "w")
+  end
 
-  def self.encrypt(argv)
-    input_filename = argv[0]
-    output_filename = argv[1]
-  binding.pry
+  def self.start(argv)
+    read_message_from_file(argv[0])
+    create_write_file(argv[1])
+    if argv.length == 2
+      self.encrypt
+    else
+      self.decrypt(argv)
+    end
+  end
+
+  def self.encrypt
+    enigma = Enigma.new
+    encrypted = enigma.encrypt(@input)
+    @output_file.write(encrypted[:encryption])
+    @output_file.close
+    puts "Created #{@output_name} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
+  end
+
+  def self.decrypt(argv)
+    enigma = Enigma.new
+    decrypted = enigma.decrypt(@input, argv[2], argv[3])
+    @output_file.write(decrypted[:decryption])
+    @output_file.close
+    puts "Created #{@output_name} with the key #{decrypted[:key]} and date #{decrypted[:date]}"
   end
 
 end
