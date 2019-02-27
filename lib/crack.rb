cb0ebad2c3570dd7bc5e9178cb5b189ae265b0a7
Matt Weiss <weiss.matt@outlook.com>
2019-02-26 17:20:22 -0700

    Change crack to runner file

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,42 +1,8 @@
-module Crack
+require './lib/runner'
 
-  def find_key
-
-  end
-
-  def find_shift_1(message)
-    key_character = message[-4]
-    (26 - character_set.find_index(key_character))
-  end
-
-  def find_shift_2(message)
-    key_character = message[-3]
-    (4 - character_set.find_index(key_character))
-  end
-
-  def find_shift_3(message)
-    key_character = message[-2]
-    (13 - character_set.find_index(key_character))
-  end
-
-  def find_shift_4(message)
-    key_character = message[-1]
-    (3 - character_set.find_index(key_character))
-  end
-
-  def crack_shifts(message)
-    shift = {}
-    shift[:a] = find_shift_1(message)
-    shift[:b] = find_shift_2(message)
-    shift[:c] = find_shift_3(message)
-    shift[:d] = find_shift_4(message)
-    shift
-  end
-
-  def shifts_minus_offsets(message, date)
-    crack_shifts(message).merge(offsets(date)) do |hash_key, shift, offset|
-      shift - offset
-    end
-  end
-
+if ARGV.length != 2 && ARGV.length != 3
+  puts "Expected 2-3 arguments, received #{ARGV.length}"
+  exit
 end
+
+Runner.start(ARGV)
